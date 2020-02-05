bce629adec8e649191204e7b6cbbb6db5c87cd2c
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-02-05 15:55:26 +0100

    Create model for class Video

---
+++ app/Video.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    //
+}
+
+Class Videotest extends Model
+{
+    protected $table = "videos";
+
+    private $id;
+    private $youtube_uid;
+    private $title;
+    private $description;
+    private $category;
+    private $reviews;
+    private $available_to_watch;
+    private $created_by;
+
+    public function review_video(){
+
+    };
+    
+    public function upload_by_employee(){
+
+    };
+
+    public function upload_video_by_guest(){
+
+    };
+
+    public function update_video(){
+
+    };
+
+    public function delete_video(){
+
+    };
+
+    public function getAveragerating(){
+
+    };
+};
