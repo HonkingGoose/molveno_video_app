2c29ed3ff38f2a66d4cd87b74cf457a59d98d3d9
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-02-05 14:51:35 +0100

    Add route and view for videos

---
+++ resources/views/videos.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Videos</title>
+</head>
+<body>
+    <h1>Watch Videos</h1>
+    <iframe width="800" height="500" src="https://www.youtube.com/embed/rLe_LjlZHOg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</body>
+</html>
