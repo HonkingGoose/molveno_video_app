cf87fffacde36c894caa12da661500385121ce8b
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-02-05 16:28:35 +0100

    Create database migration for video table WIP

---
+++ database/migrations/2020_02_05_145603_create_videos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->youtube_uid()->unique();
+            $table->title();
+            $table->description();
+            $table->category();
+            $table->reviews();
+            $table->available_to_watch();
+            $table->created_by();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
