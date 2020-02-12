0fdddbafbe07949a4a541fdf394ca8dbd9bdce3b
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-02-12 16:05:39 +0100

    Remove properties from class

--- app/Video.php
+++ app/Video.php
@@ -6,32 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
-    protected $table = "videos";
-
-    private $id;
-    private $youtube_uid;
-    private $title;
-    private $description;
-    private $category;
-    private $reviews;
-    private $available_to_watch;
-    private $suitable_for_kids;
-    private $created_by;
-
-    public function reviewVideo()
-    {
-    }
-    
-    public function uploadByEmployee()
-    {
-    }
-
-    public function uploadVideoByGuest()
-    {
-    }
-
-    public function getAverageRating()
-    {
-    }
-
 }
