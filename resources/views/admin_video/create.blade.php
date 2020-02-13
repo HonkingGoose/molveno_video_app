6f0bfd9ae26d6ab45366ca51242b3d52020621c1
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-02-13 13:35:28 +0100

    Create form (still WIP)

--- resources/views/admin_video/create.blade.php
+++ resources/views/admin_video/create.blade.php
@@ -8,9 +8,40 @@
 @endsection
 
 @section('content')
-<h2>Add video</h2>
-<div>
-    @foreach ($name as $name)
-    @endforeach
-</div>
+    <div>
+        <h2>Create video</h2>
+        <form method="POST">
+            @csrf
+
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+
+            <label for="title">Title:</label>
+                <input id="title" type="text" name="title">
+                <br>
+            <label for="description">Description:</label>
+                <input id="description" type="text">
+                <br>
+
+                <label for="category-select">Choose a category:</label>
+                    <select name="category" id="category-select">
+                        <option value="">--Please choose an option--</option>
+                        @foreach ($categories as $category)
+                        <option value="{{ $category }}">{{ $category }}</option>
+                        @endforeach
+                    </select>
+                <br>
+                <!-- To build: available_to_watch checkbox
+                    suitable_for_kids checkbox
+                    textbox for YouTube uid -->
+            <button class="btn btn-primary">Create video</button>
+        </form>
+    </div>
 @endsection
