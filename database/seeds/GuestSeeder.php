89d10c9e25efee216faba66137ad71db251ae565
dent1p <48087345+dent1p@users.noreply.github.com>
2020-02-13 10:44:43 +0100

    Create seeder + facotry for Guest class

---
+++ database/seeds/GuestSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GuestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        (new Faker\Generator)->seed(100);
+        factory(App\Guest::class, 10)->create();
+    }
+}
