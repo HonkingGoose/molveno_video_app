ea02ccc1bff400f10e03ac0034547ea38e480e0a
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-02-17 11:28:50 +0100

    Create some CRUD tests for admin video
    - Remove example test from feature directory
    - Create 2 tests that validate views
    - Create 2 store() tests
    - Fix code with Codesniffer

---
+++ tests/Feature/AdminVideoTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Video;
+use Database\Factories\VideoFactory;
+
+class AdminVideoTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+
+    public function admin_video_page_returns_a_view()
+    {
+        $response = $this->get('/admin/video');
+
+        $response->assertStatus(200);
+        $response->assertViewIs('admin_video.index');
+    }
+
+    /**
+     * @test
+     */
+
+    public function admin_video__create_page_returns_a_view()
+    {
+        $response = $this->get('/admin/video/create');
+
+        $response->assertStatus(200);
+        $response->assertViewIs('admin_video.create');
+    }
+
+    /**
+     * @test
+     */
+
+    public function store_with_empty_data_fails()
+    {
+        $response = $this->post(
+            'admin/video/1', $data = [
+
+            ]
+        );
+
+        $response->assertStatus(500);
+
+    }
+
+    /**
+     * @test
+     */
+
+    public function store_succes_saves_to_database_and_redirects()
+    {
+        $data = factory(Video::class)->make()->toArray();
+
+        // TODO: Figure out how to get ID of just created entry
+
+        $response = $this->post('admin/video/{video}', $data);
+        $this->assertDatabaseHas('videos', $data);
+
+        $response->assertRedirect('admin_video.index');
+    }
+
+}
