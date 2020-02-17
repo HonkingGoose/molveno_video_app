fe356ce0ff7b7e06ac7593a0acf3dc4728bbcc5d
mnk87 <monk87@gmail.com>
2020-02-17 12:25:58 +0100

    modify show template for video

--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -8,9 +8,66 @@
 @endsection
 
 @section('content')
-    <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/{{ $video->uid }}?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
+<div id="player"></div>
+
+<script>
+  // 2. This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: '{{ $video->youtube_uid }}',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  
+  function onPlayerStateChange(event) {
+    
+  }
+  
+
+  document.addEventListener('keyup', (event) => {
+      // keycode 13 = enter
+      if(event.keyCode === 13){
+          startstop(event);
+      }
+  });
+
+  function startstop(event){
+      // playerstate 1 = playing
+      if(player.getPlayerState() === 1){
+          player.stopVideo();
+          document.exitFullscreen();
+      } else {
+        document.getElementById('player').requestFullscreen();
+        player.playVideo();
+      
+      }
+      
+  }
+</script>
+
     <div>
         <p>title: {{ $video->title }}</p>
         <p>description: {{ $video->description }}</p>
