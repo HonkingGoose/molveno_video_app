9195b9d548e42a4da1274eeab63f8375cbf012a2
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-02-19 16:16:55 +0100

    Create non-functional test

---
+++ tests/Unit/VideoTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutEvents;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Video;
+use Illuminate\Support\Facades\DB;
+
+class VideoTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * @test
+     */
+
+    public function creating_a_record_is_saved_to_the_database()
+    {
+        // $this->withoutExeceptionHandling();
+        $video = factory(Video::class)->create();
+        print_r($video);
+
+        $response = DB::table('videos')->get();
+        print_r($response);
+
+        $this->assertDatabaseHas('videos', $video);
+
+    }
+}
