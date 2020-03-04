9f0342950b1a6f5e7b0de433d29d228a62e1a508
mnk87 <monk87@gmail.com>
2020-03-04 16:33:58 +0100

    Create userHash function

--- app/Guest.php
+++ app/Guest.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Guest extends Model
 {
-    //
+    public function generateUserHash()
+    {
+        return hash("sha256", $this->id . $this->salt);
+    }
 }
--- database/factories/RatingsFactory.php
+++ database/factories/RatingsFactory.php
@@ -6,9 +6,15 @@ use App\Rating;
 use Faker\Generator as Faker;
 
 $factory->define(Rating::class, function (Faker $faker) {
+    // dit gaan we misschien ooit nog gebruiken, voor nu gaan we de seeder hardcoden
+    // $videoId = \App\Video::all()->random()->id;
+    // $userHash = \App\Guest::all()->random()->generateUserHash();
+
+    // DB::table('ratings')->select('1')->where("video_id", $videoId)->andWhere("user_hash", $userHash);
+
     return [
         'score' => $faker->numberBetween($min = 1, $max = 5),
         'video_id' => 1,
-        'user_hash' => rand()
+        'user_hash' => 
     ];
 });
