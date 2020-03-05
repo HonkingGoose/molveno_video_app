6e2af98ca472db821853549994ae575d8635baba
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-03-05 11:21:53 +0100

    Create seeder for ratings plus small fixes
    - Use random number for userhash
    - Comment out code

--- database/seeds/RatingsTableSeeder.php
+++ database/seeds/RatingsTableSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Guest;
+use App\Rating;
+use App\Video;
 use Illuminate\Database\Seeder;
 
 class RatingsTableSeeder extends Seeder
@@ -11,7 +14,23 @@ class RatingsTableSeeder extends Seeder
      */
     public function run()
     {
-        (new Faker\Generator)->seed(123);
-        factory(App\Rating::class, 3)->create();
+        // We're not using this factory at the moment.
+        // (new Faker\Generator)->seed(123);
+        // factory(App\Rating::class, 3)->create();
+
+        // Get list of guests
+        $guests = Guest::all();
+        // Get lists of videos
+        $videos = Video::all();
+        // Attach a rating to each video for each guest.
+        foreach ($videos as $video) {
+            foreach ($guests as $guest) {
+                $rating = new Rating;
+                $rating->video_id = $video->id;
+                $rating->user_hash = $guest->generateUserHash();
+                $rating->score = rand($min =1, $max = 5);
+                $rating->save();
+            }
+        }
     }
 }
