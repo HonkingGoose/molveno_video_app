a4c6a5de142a3dcb64b2d07e17680a070bf702df
Marc Theunissen <mtheunissen82@gmail.com>
2020-03-18 16:14:40 +0100

    Add logic for handling duplicate ratings

--- app/Rating.php
+++ app/Rating.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
-    //
+    public static function findByVideoIdAndUserHash($videoId, $userHash)
+    {
+        return Rating::where(
+            [
+                'video_id' => $videoId,
+                "user_hash" => $userHash
+            ]
+        )->first();
+    }
 }
