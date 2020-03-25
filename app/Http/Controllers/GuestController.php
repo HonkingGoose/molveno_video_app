a6ef57b321a824bd85ce5325003f206d97648c44
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-03-25 10:52:00 +0100

    Add new TODO

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -37,6 +37,7 @@ class GuestController extends Controller
 
         // TODO: Make if/else statement: if search is null return all videos, else only return matching videos.
         // TODO: Refactor if else statement to remove duplication.
+        // TODO: cleanup code styling.
         if (is_null($search) === true) {
             return view('video.index', ['video' => Video::all(), 'search' => $search]);
         } else {
