8fb153527516d7ad3b119dd15a55b0ae1da2efc2
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-03-25 15:33:34 +0100

    Use strict equality for comparisons

--- public/js/thumbnailIndex.js
+++ public/js/thumbnailIndex.js
@@ -13,29 +13,29 @@ $(document).ready(function(){
     // User navigates table using keyboard
     $('table').keydown(function (e) {
         let c = "";
-        if (e.which == 39) {
+        if (e.which === 39) {
             // Right Arrow
             c = currCell.next();
-        } else if (e.which == 37) {
+        } else if (e.which === 37) {
             // Left Arrow
             c = currCell.prev();
-        } else if (e.which == 38) {
+        } else if (e.which === 38) {
             // Up Arrow
             c = currCell.closest('tr').prev().find('td:eq(' +
             currCell.index() + ')');
-        } else if (e.which == 40) {
+        } else if (e.which === 40) {
             // Down Arrow
             c = currCell.closest('tr').next().find('td:eq(' +
             currCell.index() + ')');
-        } else if (!editing && (e.which == 13 || e.which == 32)) {
+        } else if (!editing && (e.which === 13 || e.which === 32)) {
             // Enter or Spacebar - edit cell
            const link = currCell.find("a").attr("href");
            window.location.href = link;
-        } else if (!editing && (e.which == 9 && !e.shiftKey)) {
+        } else if (!editing && (e.which === 9 && !e.shiftKey)) {
             // Tab
             e.preventDefault();
             c = currCell.next();
-        } else if (!editing && (e.which == 9 && e.shiftKey)) {
+        } else if (!editing && (e.which === 9 && e.shiftKey)) {
             // Shift + Tab
             e.preventDefault();
             c = currCell.prev();
