811c08f2848e45cb2fb084ffc759476ee3f8e78f
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-03-30 15:23:15 +0200

    Change success message to a simpler variant

--- resources/views/contact/sentSuccessfully.blade.php
+++ resources/views/contact/sentSuccessfully.blade.php
@@ -1,8 +1,7 @@
 @extends('layout.app')
 
-
 @section('title', 'contact form send successfully')
 
 @section('content')
-<h1> Your form has been sent successfully <h1>
+<h1>Your contact form has been received.<h1>
 @endsection
