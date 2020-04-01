354e768a05bd00ff6ce34e242b343093d4b7debd
mnk87 <monk87@gmail.com>
2020-04-01 11:16:43 +0200

    Add contactForm blade template

---
+++ resources/views/mail/contact.blade.php
@@ -0,0 +1,9 @@
+<h1>{{ $contactForm->firstName }} {{ $contactForm->lastName }} has said the following:</h1>
+
+<p>from roomnumber: {{ $contactForm->roomNumber }}</p>
+
+<p>category: {{ $contactForm->category }}</p>
+<p>created at: {{ $contactForm->created_at }}</p>
+<h2>message</h2>
+<p>{{ $contactForm->message }}</p>
+
