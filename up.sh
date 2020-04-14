6a5c9ce3ccd5a162033d8add9175f42b6ad12861
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2020-04-14 14:27:44 +0200

    Small fixes:
    - Change word order
    - Add punctuation
    - Fix indentation

--- up.sh
+++ up.sh
@@ -8,22 +8,22 @@ db_directory="run/var"
 
 # Test variables are present that will be used later in the script:
 if [ -z ${default_username} ]; then
-    echo -e "ERROR: shell script 'default_username' variable is empty."
+    echo -e "ERROR: shell script variable 'default_username' is empty."
     exit
 fi
 
 if [ -z ${default_password} ]; then
-    echo -e "ERROR: shell script 'default_password' variable is empty."
+    echo -e "ERROR: shell script variable 'default_password' is empty."
     exit
 fi
 
 if [ -z ${docker_database_password} ]; then
-    echo -e "ERROR: shell script 'docker_database_password' variable is empty."
+    echo -e "ERROR: shell script variable 'docker_database_password' is empty."
     exit
 fi
 
 if [ -z ${db_directory} ]; then
-    echo -e "ERROR: shell script variable 'db_directory' is empty"
+    echo -e "ERROR: shell script variable 'db_directory' is empty."
     exit
 fi
 
@@ -32,12 +32,15 @@ if [[ ! -f .env ]]; then
   echo -e "STEP: creating .env file"
 
   cp -v .env.example .env
+
   sed -i 's/DB_HOST=.*/DB_HOST=mysql-db/' .env
   sed -i 's/DB_DATABASE=.*/DB_DATABASE=molveno/' .env
   sed -i "s/DB_USERNAME=.*/DB_USERNAME=${default_username}/" .env
   sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=${default_password}/" .env
   sed -i "s/DB_ROOT_PASSWORD_FOR_DOCKER=.*/DB_ROOT_PASSWORD_FOR_DOCKER=${docker_database_password}/" .env
+
   echo -e "UID=${UID}" >> .env
+
   echo -e "WARNING: Please change the default passwords in the .env file."
   echo -e "WARNING: Then run the script again."
   exit
@@ -53,7 +56,7 @@ if [ -d ${db_directory} ]; then
     elif [ -d ${db_directory} ]; then
         echo -e "\nERROR: Could not delete the database directory, exiting script."
         echo -e "INFO: Please delete the ${db_directory} directory manually."
-    exit
+        exit
 fi
 
 echo -e "\nSTEP: clean up previous docker-compose containers"
